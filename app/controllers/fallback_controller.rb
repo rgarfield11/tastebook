5d3300d431eb79f41512d02a036d744442e04466
Ross <rossgarfield@gmail.com>
2022-08-03 13:25:41 -0500

    fallback ctrlr

---
+++ app/controllers/fallback_controller.rb
@@ -0,0 +1,7 @@
+class FallbackController < ApplicationController::Base
+
+    def index
+        render file: 'public/index.html'
+      end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get '*path', to: "fallback#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
