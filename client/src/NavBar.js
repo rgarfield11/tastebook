7ed0eb6d7b62303cec4bca4a4cfbba5ba15905f7
Ross <rossgarfield@gmail.com>
2022-08-04 12:32:47 -0500

    fix mobile format

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -33,10 +33,12 @@ function NavBar({ setUser }) {
 
   return (
     <div className="navBar">
-      <h2 id="logo" className="logo" style={navStyle} onClick={handleReturnHome}>tastebook</h2> &nbsp;
-      <button  className="navButton" style={navStyle} onClick={handleNewRecipe}>Add Recipe</button> &nbsp;
-      <button  className="navButton" style={navStyle} onClick={handleProfileRoute}>Profile</button> &nbsp;
-      <button  className="navButton" style={navStyle} onClick={handleLogout}>Logout</button> &nbsp;
+      <div className="navBar_content">
+        <h2 id="logo" className="logo" style={navStyle} onClick={handleReturnHome}>tastebook</h2> &nbsp;
+        <button  className="navButton" style={navStyle} onClick={handleNewRecipe}>Add Recipe</button> &nbsp;
+        <button  className="navButton" style={navStyle} onClick={handleProfileRoute}>Profile</button> &nbsp;
+        <button  className="navButton" style={navStyle} onClick={handleLogout}>Logout</button> &nbsp;
+      </div>
     </div>
   )
 }
